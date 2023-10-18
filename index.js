8cc5d84f5e11148711ab4e73b490289da35605f3
Rifazul <rifazul50@gmail.com>
2023-10-18 08:28:39 +0600

    add the category data get

--- index.js
+++ index.js
@@ -27,14 +27,17 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  const categoryCollection = client.db("automotivesDB").collection("categories")
+ 
+  // get method used 
+  app.get("/categories",async(req,res)=>{
+    const allBrands = categoryCollection.find();
+    const result = await allBrands.toArray();
+    res.send(result)
+  })
+
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    
   }
 }
 run().catch(console.dir);
